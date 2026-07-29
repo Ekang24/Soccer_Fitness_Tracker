4b14937dea014b70d361c58fc2348d751089a3ad
Ekang24 <eugenek24@outlook.com>
2026-07-29 18:02:35 -0400

    Data storge in JSON

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import json
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,11 +16,27 @@ def add_workout():
         difficulty = request.form["difficulty"]
         notes = request.form["notes"]
 
+        workout = {
+            "date": date,
+            "distance": distance,
+            "time": time,
+            "difficulty": difficulty,
+            "notes": notes
+        }
+
         print(date)
         print(distance)
         print(time)
         print(difficulty)
         print(notes)
+
+        with open("workouts.json", "r") as file:
+            workouts = json.load(file)
+
+        workouts.append(workout)
+
+        with open("workouts.json", "w") as file:
+            json.dump(workouts, file, indent=4)
 
     return render_template("add_workout.html")
 
